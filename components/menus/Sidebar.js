3618c2eb8f318db8a7eae4a9484dc047d9c96e2a
Karen Molina <marcesophi1003@gmail.com>
2025-12-08 20:42:34 -0500

    Some adjustments to the sidebar

--- components/menus/Sidebar.js
+++ components/menus/Sidebar.js
@@ -35,47 +35,47 @@ const Sidebar = () => {
             href: "/module4",
             name: "Pensiones",
             icon: (
-                <Icon icon="fluent-mdl2:product" width="24" height="24" />
+                <Icon icon="healthicons:old-man-outline" width="24" height="24" />
             ),
         },
         {
             href: "/module5",
             name: "Administración de salarios",
-            icon: <Icon icon="fluent-mdl2:product" width="24" height="24" />,
+            icon: <Icon icon="vaadin:cash" width="24" height="24" />,
         },
         {
             href: "/module6",
             name: "Seguridad y salud en el trabajo",
-            icon: <Icon icon="fluent-mdl2:product" width="24" height="24" />,
+            icon: <Icon icon="ix:helmet-safety" width="24" height="24" />,
         },
         {
             href: "/module7",
             name: "Administración de documentos",
             icon: (
-                <Icon icon="fluent-mdl2:product" width="24" height="24" />
+                <Icon icon="ph:files-duotone" width="24" height="24" />
             ),
         },
         {
             href: "#",
             name: "Vivienda",
             icon: (
-                <Icon icon="fluent-mdl2:product" width="24" height="24" />
+                <Icon icon="mdi:house-outline" width="24" height="24" />
             ),
         },
         {
             href: "#",
             name: "Relaciones sindicales",
-            icon: <Icon icon="fluent-mdl2:product" width="24" height="24" />,
+            icon: <Icon icon="streamline-ultimate:labor-hands-action" width="24" height="24" />,
         },
         {
             href: "#",
             name: "Servicio médico asistencial",
-            icon: <Icon icon="fluent-mdl2:product" width="24" height="24" />,
+            icon: <Icon icon="fluent:doctor-48-regular" width="24" height="24" />,
         },
         {
             href: "#",
             name: "Gestión administrativa",
-            icon: <Icon icon="fluent-mdl2:product" width="24" height="24" />,
+            icon: <Icon icon="clarity:administrator-line" width="24" height="24" />,
         },
     ];
 
@@ -83,7 +83,7 @@ const Sidebar = () => {
         <nav className="fixed top-10 left-0 pt-14 h-full bg-gray-50 flex flex-col w-20 sm:w-84 font-work-sans">
             <div className="flex-1 overflow-y-auto">
                 <ul className="px-2 sm:px-7 font-medium text-base flex-1 text-neutral-800">
-                    <li className="my-2">
+                    <li className="my-3">
                         <button
                             onClick={() => setOpenDropdown(!openDropdown)}
                             className="flex items-center gap-x-3 py-3 px-3 rounded-lg justify-center sm:justify-start duration-150 hover:bg-[#F2F2F3] hover:scale-105 hover:translate-x-1 w-full"
@@ -106,7 +106,7 @@ const Sidebar = () => {
                                             <Link
                                                 href={child.href}
                                                 className={`flex items-center gap-x-2 py-2 px-2 rounded-lg duration-150 hover:bg-[#F2F2F3]
-                          ${isActive ? "bg-linear-to-r from-lime-600 to-[#39A900] text-white shadow-md" : ""}`}
+                                                ${isActive ? "bg-linear-to-r from-lime-600 to-[#39A900] text-white shadow-md" : ""}`}
                                             >
                                                 {child.icon}
                                                 <span className="hidden sm:inline">{child.name}</span>
@@ -117,18 +117,15 @@ const Sidebar = () => {
                             </ul>
                         )}
                     </li>
-
-                    {/* El resto de items */}
                     {navigation.slice(1).map((item, idx) => {
-
                         const isActive = pathname === item.href;
                         return (
                             <li key={idx} className="my-2">
                                 <Link
                                     href={item.href}
                                     className={`flex items-center gap-x-3 py-3 px-3 rounded-lg justify-center sm:justify-start duration-150
-                    hover:bg-[#F2F2F3] hover:scale-105 hover:translate-x-1
-                    ${isActive ? "bg-linear-to-r from-lime-600 to-[#39A900] text-white shadow-md shadow-indigo-500/20" : ""}`}
+                                    hover:bg-[#F2F2F3] hover:scale-105 hover:translate-x-1
+                                    ${isActive ? "bg-linear-to-r from-lime-600 to-[#39A900] text-white shadow-md shadow-indigo-500/20" : ""}`}
                                 >
                                     <div>{item.icon}</div>
                                     <span className="hidden sm:inline">{item.name}</span>
